89e7b941f7dc108164bfdd2ecc389b4e37d299f5
Naz Rejepov <nazar.rejepov@qa.com>
2026-02-27 17:08:23 +0000

    Update Helm repository URL for k8s dashboard

--- code/k8s-dash/install.sh
+++ code/k8s-dash/install.sh
@@ -3,7 +3,7 @@ echo installing k8s dashboard...
 
 kubectl create ns k8sdash
 
-helm repo add k8s-dashboard https://kubernetes.github.io/dashboard
+helm repo add k8s-dashboard https://kubernetes-retired.github.io/dashboard
 helm repo update
 
 helm install k8s-dashboard k8s-dashboard/kubernetes-dashboard \
